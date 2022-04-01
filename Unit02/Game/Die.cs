760db27f9c402b41347c516fe5dfd93852c9fd5a
Nate Phillips <knathanp@gmail.com>
2022-04-01 10:05:38 -0600

    finish dice game

--- Unit02/Game/Die.cs
+++ Unit02/Game/Die.cs
@@ -6,26 +6,53 @@ namespace Unit02.Game
     // TODO: Implement the Die class as follows...
     // 1) Add the class declaration. Use the following class comment.
 
-        /// <summary>
-        /// A small cube with a different number of spots on each of its six sides.
-        /// 
-        /// The responsibility of Die is to keep track of its currently rolled value and the points its
-        /// worth.
-        /// </summary> 
-
-
-    // 2) Create the class constructor. Use the following method comment.
+    /// <summary>
+    /// A small cube with a different number of spots on each of its six sides.
+    /// 
+    /// The responsibility of Die is to keep track of its currently rolled value and the points its
+    /// worth.
+    /// </summary> 
+    public class Die
+    {
+        // 2) Create the class constructor. Use the following method comment.
 
         /// <summary>
         /// Constructs a new instance of Die.
         /// </summary>
+        public Die()
+        {
+            value = 0;
+            points = 0;
+        }
 
-    
-    // 3) Create the Roll() method. Use the following method comment.
-        
+        // 3) Create the Roll() method. Use the following method comment.
+
         /// <summary>
         /// Generates a new random value and calculates the points for the die. Fives are 
         /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
         /// </summary>
-        
+        public void Roll()
+        {
+            Random random = new Random();
+            value = random.Next(1, 7); // roll dice by selecting a number 1-6
+            
+            if (value == 5)
+                points = 50;
+            else if (value == 1)
+                points = 100;
+            else
+                points = 0;
+        }
+
+        /// <summary>
+        /// Currently rolled value.
+        /// </summary>
+        public int value;
+
+        /// <summary>
+        /// The points this value is worth.
+        /// </summary>
+        public int points;
+
+    }
 }
