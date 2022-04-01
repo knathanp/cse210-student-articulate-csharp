4334186d0a44fa4b1f5a16accd396a1974b3c1db
Nate Phillips <knathanp@gmail.com>
2022-04-01 10:36:20 -0600

    finish RFK game

--- Unit04/Game/Casting/Artifact.cs
+++ Unit04/Game/Casting/Artifact.cs
@@ -5,34 +5,48 @@ namespace Unit04.Game.Casting
     // 1) Add the class declaration. Use the following class comment. Make sure you
     //    inherit from the Actor class.
 
-        /// <summary>
-        /// <para>An item of cultural or historical interest.</para>
-        /// <para>
-        /// The responsibility of an Artifact is to provide a message about itself.
-        /// </para>
-        /// </summary>
+    /// <summary>
+    /// <para>An item of cultural or historical interest.</para>
+    /// <para>
+    /// The responsibility of an Artifact is to provide a message about itself.
+    /// </para>
+    /// </summary>
+    public class Artifact : Actor
+    {
+        private string _message = "";
 
+        // 2) Create the class constructor. Use the following method comment.
 
-    // 2) Create the class constructor. Use the following method comment.
-        
         /// <summary>
         /// Constructs a new instance of Artifact.
         /// </summary>
-       
+        public Artifact()
+        {
+            _message = "";
+        }
 
-    // 3) Create the GetMessage() method. Use the following method comment.
-        
+
+        // 3) Create the GetMessage() method. Use the following method comment.
+
         /// <summary>
         /// Gets the artifact's message.
         /// </summary>
         /// <returns>The message as a string.</returns>
-        
+        public string GetMessage()
+        {
+            return _message;
+        }
 
-    // 4) Create the SetMessage(string message) method. Use the following method comment.
-        
+
+        // 4) Create the SetMessage(string message) method. Use the following method comment.
+
         /// <summary>
         /// Sets the artifact's message to the given value.
         /// </summary>
         /// <param name="message">The given message.</param>
-    
+        public void SetMessage(string message)
+        {
+            _message = message;
+        }
+    }
 }
