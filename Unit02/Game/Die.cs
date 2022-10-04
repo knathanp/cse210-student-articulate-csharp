c10087722bb694cbad58120ac56772428122624e
Nate Phillips <knathanp@gmail.com>
2022-10-04 09:54:08 -0600

    finish Dice game

--- Unit02/Game/Die.cs
+++ Unit02/Game/Die.cs
@@ -3,29 +3,23 @@ using System;
 
 namespace Unit02.Game
 {
-    // TODO: Implement the Die class as follows...
-    // 1) Add the class declaration. Use the following class comment.
+    public class Die
+    {
+        public int value;
+        public int points;
 
-        /// <summary>
-        /// A small cube with a different number of spots on each of its six sides.
-        /// 
-        /// The responsibility of Die is to keep track of its currently rolled value and the points its
-        /// worth.
-        /// </summary> 
+        public Die() 
+        {
+            Roll();
+        }
 
+        public void Roll()
+        {
 
-    // 2) Create the class constructor. Use the following method comment.
-
-        /// <summary>
-        /// Constructs a new instance of Die.
-        /// </summary>
-
-    
-    // 3) Create the Roll() method. Use the following method comment.
-        
-        /// <summary>
-        /// Generates a new random value and calculates the points for the die. Fives are 
-        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
-        /// </summary>
+            Random r = new Random();
+            value = r.Next(1, 7);
+            points = (value == 1) ? 100 : (value == 5) ? 50 : 0;
+        }
+    }
         
 }
